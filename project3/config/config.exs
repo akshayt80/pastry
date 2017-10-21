6e93576aaed71adf525268b229ef6c3ac692bd1a
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-10-21 04:01:35 -0400

    using logger at debug level

--- project3/config/config.exs
+++ project3/config/config.exs
@@ -18,7 +18,9 @@ use Mix.Config
 #
 # You can also configure a 3rd-party app:
 #
-#     config :logger, level: :info
+config :logger,
+    backends: [:console]
+    level: :debug
 #
 
 # It is also possible to import configuration files, relative to this
