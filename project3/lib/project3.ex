c6f0b4898975cfc7331ca73c4d4ffb1c7e759854
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-10-21 03:58:20 -0400

    accepting number of nodes and number of request from commandline

--- project3/lib/project3.ex
+++ project3/lib/project3.ex
@@ -12,7 +12,14 @@ defmodule Project3 do
       :world
 
   """
-  def hello do
-    :world
+  def main(args) do
+      {_, [nodes, requests], _} = OptionParser.parse(args)
+      IO.puts "command line arguments: #{inspect(nodes)}"
+      nodes = elem(Integer.parse(nodes), 0)
+      IO.puts "command line arguments: #{inspect(requests)}"
+      requests = elem(Integer.parse(requests), 0)
+      TempMaster.init()
+      #bit = NodeLogic.different_bit("FE9FCB37C30201F73F142449D037028D", "FE9FC289C3FF0AF142B6D3BEAD98A923")
+      #IO.puts "bits differ in two strings at: #{bit}"
   end
 end
