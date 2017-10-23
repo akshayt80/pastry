1999facbdcb6540ba29a97cc89f7730fa0fc7015
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-10-23 13:45:58 -0400

    enabled logger and calling master node

--- project3/lib/project3.ex
+++ project3/lib/project3.ex
@@ -12,14 +12,19 @@ defmodule Project3 do
       :world
 
   """
+  require Logger
   def main(args) do
       {_, [nodes, requests], _} = OptionParser.parse(args)
       IO.puts "command line arguments: #{inspect(nodes)}"
       nodes = elem(Integer.parse(nodes), 0)
       IO.puts "command line arguments: #{inspect(requests)}"
       requests = elem(Integer.parse(requests), 0)
-      TempMaster.init()
-      #bit = NodeLogic.different_bit("FE9FCB37C30201F73F142449D037028D", "FE9FC289C3FF0AF142B6D3BEAD98A923")
-      #IO.puts "bits differ in two strings at: #{bit}"
+      #TempMaster.init()
+      # bit = NodeLogic.different_bit("12632", "42832", 5)
+      # Logger.info "bits differ in two strings at: #{bit}"
+      # spawn fn -> Master.init(nodes, requests) end
+      # :timer.sleep 1000
+      # send :master, {:start, "start"}
+      Master.init(nodes, requests)
   end
 end
